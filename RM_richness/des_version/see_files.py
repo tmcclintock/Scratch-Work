586be316d2c0150217d88d73e32d13da345c4770
Tom McClintock <tmcclintock@email.arizona.edu>
2017-03-08 09:12:12 -0700

    Working on the DES version, but I don't have access to the redshift lists, and it isn't clear which Z_LAMBDA to use.

--- RM_richness/des_version/see_files.py
+++ RM_richness/des_version/see_files.py
@@ -9,4 +9,7 @@ fname = "y1a1_gold_1.0.3_wide+d10-mof-001b_run_runpos.fit"
 data,header = fitsio.read(fname,header=True)
 for name in data.dtype.names: print name
 z = data['Z_LAMBDA_IN']
-print z.shape
+zt = data['Z_LAMBDA']
+print z.shape, zt.shape
+print min(z),max(z)
+print min(zt),max(zt)
