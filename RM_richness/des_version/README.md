b0981666b676ba28bfdaa9fc072aeb318c67b22e
Tom McClintock <tmcclintock89@gmail.com>
2017-03-08 18:33:05 -0700

    Got rotations down.

---
+++ RM_richness/des_version/README.md
@@ -0,0 +1,9 @@
+DES version of Redmapper Richnesses
+===================================
+Redmapper clusters are defined by their redshift and richnesses.
+Eli has provided some fits files with a list of clusters, their
+most likely richnesses (at some redshift) as well as a list
+of measured richnesses assuming different redshifts.
+
+My goal is to measure the width of the distribution, assuming a
+gaussian, around the most likely redshifts.
--- rotate_params/work.py
+++ rotate_params/work.py
@@ -10,7 +10,7 @@ data = np.loadtxt("data.txt")
 
 #Expectation values
 eZ = np.mean(data,0)
-D = data[:] - eZ
+D = data[:]# - eZ
 d,g = D[:,0],D[:,1]
 
 #Covariance matrix of D
@@ -18,11 +18,12 @@ C= np.cov(D,rowvar=False)
 #Get the eigenvalues and eigenvectors
 w,v = np.linalg.eig(C)
 i = np.argmax(w)
-v = v[:,i]
-x = np.linspace(min(d),max(d),1000)
-y =  v[1]*x# + v[0]
 print v
+rD = np.dot(D[:],v)
+rrD = np.dot(rD[:],v.T)
+a,b = rD.T
+print D[0],rrD[0]
 
-plt.scatter(d,g,marker=".",alpha=0.1)
-plt.plot(x,y)
+plt.scatter(a,b,marker=".",alpha=0.1)
+#plt.scatter(d,g,marker='.',alpha=0.1)
 plt.show()
