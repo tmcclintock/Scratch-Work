0acee54caf73c5ff2d63242fdf60e7d1cd818c5d
Tom McClintock <tmcclintock@email.arizona.edu>
2017-04-17 14:12:39 -0700

    Investigating the error in corner.py that won't work with numpy 1.12.1

---
+++ corner_error/README.md
@@ -0,0 +1,1 @@
+I upgraded some stuff on calvin and now corner plot is broken. Gotta fix.
---
+++ corner_error/demo.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import corner
+
+# Set up the parameters of the problem.
+ndim, nsamples = 3, 50000
+
+# Generate some fake data.
+data1 = np.random.randn(ndim * 4 * nsamples / 5).reshape([4 * nsamples / 5,
+                                                          ndim])
+data2 = (4 * np.random.rand(ndim)[None, :] +
+         np.random.randn(ndim * nsamples / 5).reshape([nsamples / 5, ndim]))
+data = np.vstack([data1, data2])
+
+
+# Plot it.
+figure = corner.corner(data, labels=[r"$x$", r"$y$", r"$\log \alpha$",
+                                     r"$\Gamma \, [\mathrm{parsec}]$"],
+                       quantiles=[0.16, 0.5, 0.84],
+                       show_titles=True, title_kwargs={"fontsize": 12})
+figure.gca().annotate("a demo corner plot",
+                      xy=(1.0, 1.0), xycoords="figure fraction",
+                      xytext=(-20, -10), textcoords="offset points",
+                      ha="right", va="top")
+figure.savefig("demo.png", dpi=300)
+plt.show()
