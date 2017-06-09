8ce20d5315810c4e40ace800911e97ce4425d716
Tom McClintock <tmcclintock@email.arizona.edu>
2017-06-08 19:26:32 -0700

    More plot tweaking. All done now.

--- LOO_plots/rotated_LOO_single.py
+++ LOO_plots/rotated_LOO_single.py
@@ -27,10 +27,11 @@ def predict_parameters(cosmology, emu_list):
 
 xlabel  = r"$\log_{10}M\ [{\rm M_\odot}/h]$"
 y0label = r"$N/[{\rm Gpc}^3\  \log_{10}{\rm M_\odot}/h]$"
-y0label = r"$N/[{\rm Gpc}^3\  \log{\rm M_\odot}]$"
+y0label = r"$N/[{\rm Gpc}^3\  \log{\rm M}]$"
 #y0label = r"${\rm Mass\ Function}$"
 y1label = r"$\%\ {\rm Diff}$"
-y2label = r"$\frac{N-N_{emu}}{N_{emu}bG}$"
+y1label = r"$\frac{N-N_{emu}}{N_{emu}bG}$"
+y1label = r"$\frac{N-N_{emu}(1+bG\delta_0)}{N_{emu}}$"
 
 base = "/home/tmcclintock/Desktop/Github_stuff/Mass-Function-Emulator/test_data/"
 datapath = base+"N_data/Box%03d_full/Box%03d_full_Z%d.txt"
@@ -115,45 +116,65 @@ for i in range(0,1):
     emu_list = train(training_cosmos, training_data, training_errs)
     emu_model = predict_parameters(test_cosmo, emu_list)
 
+    lMs = []
+    N_datas = []
+    errs = []
+    N_bfs = []
+    bGs = []
+    delta0s = []
     for j in range(0,N_z):
-        #First plot the data.
+        #First get the data.
         data = np.loadtxt(datapath%(i, i, j))
         lM_bins = data[:,:2]
         lM = np.mean(data[:, :2], 1)
         N = data[:,2]
         cov = np.loadtxt(covpath%(i, i, j))
         err = np.sqrt(np.diagonal(cov))
-        axarr[0].errorbar(lM, N, err, marker='.', ls='', c=cmap(c[j]), alpha=1.0, label=r"$z=%.1f$"%redshifts[j])
-
         #Get emulated curves
         TMF_model = TMF.tinker_mass_function(cosmo_dict, redshifts[j])
         d,e,f,g,B = get_params(emu_model, scale_factors[j])
         TMF_model.set_parameters(d,e,f,g,B)
         N_bf = volume * TMF_model.n_in_bins(lM_bins)
-        axarr[0].plot(lM, N_bf, ls='--', c=cmap(c[j]), alpha=1.0)
-
-        #Plot the %difference
         bG = get_bG(cosmo_dict, scale_factors[j], 10**lM)
+        delta0 = (N-N_bf)/(N_bf*bG)
+        lMs.append(lM)
+        N_datas.append(N)
+        errs.append(err)
+        N_bfs.append(N_bf)
+        bGs.append(bG)
+        delta0s.append(delta0)
+    delta0s = np.concatenate(delta0s)
+    delta0 = np.median(delta0s)
+    print "box %d"%i, "delta0 = %f"%delta0
+    for j in range(0,N_z):
+        lM = lMs[j]
+        N = N_datas[j]
+        err = errs[j]
+        N_bf = N_bfs[j]
+        bG = bGs[j]
+        axarr[0].errorbar(lM, N, err, marker='.', ls='', c=cmap(c[j]), alpha=1.0, label=r"$z=%.1f$"%redshifts[j])
+        axarr[0].plot(lM, N_bf, ls='--', c=cmap(c[j]), alpha=1.0)
         dN_N = (N-N_bf)/N_bf
         dN_NbG = dN_N/bG
         edN_NbG = err/N_bf/bG
         pd  = 100.*dN_N
         pde = 100.*err/N_bf
-        #axarr[1].errorbar(lM, pd, pde, marker='.', ls='', c=cmap(c[j]), alpha=1.0)
-        if j > 0:
-            axarr[1].errorbar(lM, dN_NbG, edN_NbG, marker='.', ls='', c=cmap(c[j]), alpha=1.0)
+        eduardo = (N-N_bf*(1+bG*delta0))/N_bf
+        newerr = err/N_bf
+        #axarr[1].errorbar(lM, dN_NbG, edN_NbG, marker='.', ls='', c=cmap(c[j]), alpha=1.0)
+        axarr[1].errorbar(lM, eduardo, newerr, marker='.', ls='', c=cmap(c[j]), alpha=1.0)
     axarr[1].axhline(0, c='k', ls='-', zorder=-1)
 
     #Show
     axarr[1].set_xlabel(xlabel)
     axarr[0].set_ylabel(y0label)
-    axarr[1].set_ylabel(y2label)
+    axarr[1].set_ylabel(y1label)
     axarr[0].set_yscale('log')
     axarr[0].set_ylim(1, axarr[0].get_ylim()[1])
-    #axarr[1].set_ylim(-18, 18)
-    axarr[1].set_ylim(-0.05, 0.12)
-    leg = axarr[0].legend(loc=0, fontsize=8, numpoints=1, frameon=False)
+    axarr[1].set_ylim(-0.26, 0.26)
+    #axarr[1].set_ylim(-0.05, 0.12)
+    leg = axarr[0].legend(loc=0, fontsize=6, numpoints=1, frameon=False)
     leg.get_frame().set_alpha(0.5)
-    plt.subplots_adjust(bottom=0.15, left=0.15, hspace=0.0)
-    fig.savefig("fig_emurot.png")
+    plt.subplots_adjust(bottom=0.15, left=0.19, hspace=0.0)
+    #fig.savefig("fig_emurot.pdf")
     plt.show()
