d8708ae08e9466522595ce649e99586c2ff98927
Tom McClintock <tmcclintock89@gmail.com>
2017-07-13 12:25:39 -0400

    The pyhmc test works now. I submitted an issue on their page.

--- pyhmc_test/hmc_test.py
+++ pyhmc_test/hmc_test.py
@@ -1,10 +1,18 @@
 import numpy as np
 from pyhmc import hmc
 
-def logprob(x, inverse_variance):
-    logp = -0.5 * np.sum(inverse_variance * x**2)
-    gradient = -ivar * x
-    return logp, gradient
+def logprob(x, ivar):
+    logp = -0.5 * np.sum(ivar * x**2)
+    grad = -ivar * x
+    return logp, grad
+
+#ivar = 1. / np.random.rand(5)
+#samples = hmc(logprob, x0=np.random.randn(5), args=(ivar,), n_samples=1e4)
 
 ivar = 1. / np.random.rand(5)
+samples = hmc(logprob, x0=np.random.randn(5), args=(ivar,), n_samples=10000)
 
+import corner
+import matplotlib.pyplot as plt
+fig = corner.corner(samples)
+plt.show()
