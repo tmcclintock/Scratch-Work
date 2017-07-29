d771f7730473946507ae555fc862529a4a3d75d5
Tom McClintock <tmcclintock89@gmail.com>
2017-07-29 08:22:50 -0400

    Wrote xi_nfw.

--- lensing_scratch/nfw_spline.py
+++ lensing_scratch/nfw_spline.py
@@ -6,6 +6,10 @@ at a comparable speed to the C code.
 import numpy as np
 from scipy.interpolate import InterpolatedUnivariateSpline as IUS
 
-def rhonfw(R, M, rs):
+def xi_nfw(R, M, c):
+    R200 = (M/(4./3.*np.pi*rhom*200))**.33333333333
+    Rs = R200/c
+    fc = np.log(1+c)-c/(1+c)
+    xi = M/(4*np.pi*Rs**3*fc)/(R/Rs*(1+R/Rs)*(1+R/Rs))/rhom - 1.
     print "not implemented yet"
     return 0
